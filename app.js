5356c25a9fc53718f32a9e63b502f43a653a2541
Diego Méndez <diegomendezp@MacBook-Pro-de-Diego.local>
2021-02-10 17:50:20 +0100

    config: clean app

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ const mongoose = require('mongoose');
 const logger = require('morgan');
 const path = require('path');
 
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
 const passport = require('passport');
-const cors = require('cors');
+const cors = require('./configs/cors.config')
+const session = require('./configs/session.config');
 
 require('./configs/db.config');
 require('./configs/passport.config');
@@ -27,37 +26,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-const whitelist = ['http://localhost:3000'];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
-
-app.use(cors(corsOptions));
+app.use(cors);
 
 // Enable authentication using session + passport
-app.use(
-  session({
-    secret: 'Todo-app',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {
-      secure: false,
-      httpOnly: true,
-      maxAge: 60 * 60 * 24 * 1000
-    },
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
-      ttl: 24 * 60 * 60
-    })
-  })
-);
+app.use(session);
 app.use(passport.initialize());
 app.use(passport.session());
 
